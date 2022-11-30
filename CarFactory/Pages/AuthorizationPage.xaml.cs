8e82d1e86e5291e2a6483ebd0d26e555cf4694eb
Kamil Sharipov <wingimobile@gmail.com>
2022-11-30 12:40:41 +0300

    Initial commit

---
+++ CarFactory/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CarFactory.Pages
+{
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarFactory/Windows/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CarFactory.Pages;
+
+namespace CarFactory.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new AuthorizationPage());
+        }
+    }
+}
