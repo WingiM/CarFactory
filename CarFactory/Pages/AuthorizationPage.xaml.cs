aa2720e6f23605634906edd049d47d17d0582d68
Kamil Sharipov <wingimobile@gmail.com>
2022-12-03 23:10:59 +0300

    add employee page

--- CarFactory/Pages/AuthorizationPage.xaml.cs
+++ CarFactory/Pages/AuthorizationPage.xaml.cs
@@ -43,7 +43,7 @@ namespace CarFactory.Pages
                 case 1:
                     return new DirectorPage(); 
                 default:
-                    throw new Exception();
+                    return new EmployeePage();
             }
         }
     }
---
+++ CarFactory/Pages/EmployeePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using CarFactory.ADO;
+
+namespace CarFactory.Pages
+{
+    public partial class EmployeePage : Page
+    {
+        private List<RolePermission> _permissions;
+        public EmployeePage()
+        {
+            InitializeComponent();
+
+            _permissions = App.Connection.RolePermissions.Where(x => x.RoleId == App.AuthorizedUser.RoleId).ToList();
+            if (_permissions.Count == 0)
+                throw new Exception("Logged as a person with no permissions");
+        }
+    }
+}
